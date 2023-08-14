54931717c1b37613b7f9dab9b50d2e9f7de11be8
abel <abelzeleke5173@gmail.com>
2023-08-14 12:43:52 +0300

    first problem solved

---
+++ ProgressQuestions/isPalindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(e for e in s if e.isalnum())
+        if s == s[::-1]:
+            return True
+        return False
