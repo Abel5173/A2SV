b68114fb2b69e22ead8d55e7c326e88e84d77120
abel <abelzeleke5173@gmail.com>
2023-10-16 10:48:21 +0300

    two sum II

---
+++ ProgressQuestions/twoSumII.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        arr = []
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                arr.append(l+1)
+                arr.append(r+1)
+                break
+        return arr
