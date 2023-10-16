1c0b514c6b6a67214fc2d37d3d658bfccf3ca051
abel <abelzeleke5173@gmail.com>
2023-10-16 11:09:51 +0300

    move zeroes leetcode

---
+++ ProgressQuestions/moveZeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        s, p = 0, 0
+        while s < (len(nums)):
+            if nums[s] != 0:
+                nums[p], nums[s] = nums[s], nums[p]
+                p += 1
+            s += 1
