65913b33bf9be3bf55155614dccf72e0a448dddc
abel <abelzeleke5173@gmail.com>
2023-10-17 16:38:13 +0300

    Leetcode 189. Rotate Array

--- ProgressQuestions/numberOfPairs.py
+++ ProgressQuestions/numberOfPairs.py
@@ -1,22 +1,14 @@
 n, m = map(int, input().split())
 l1 = list(map(int, input().split()))
 l2 = list(map(int, input().split()))
-
-l, r = 0, 0
-cnt = 0
-temp = 0
-while l < n and r < m:
-    if l1[l] == l2[r]:
-        cnt += 1
-        temp += 1
-        l += 1
-    elif l1[l] > l2[r]:
-        r += 1
-        if r > 0 and r < m and l2[r] == l2[r-1]:
-            cnt += temp
-        else:
-            temp = 0
+pairs = 0
+dic = {}
+for i in l1:
+    if i in dic:
+        dic[i] += 1
     else:
-        l += 1
-
-print(cnt)
+        dic[i] = 1
+for i in l2:
+    if i in dic:
+        pairs += dic[i]
+print(pairs)
